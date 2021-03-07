3783f42c52590e0f7ac570b0d21c77108bc1752a
juliocamargo <julioccr.sm@gmail.com>
2021-03-06 21:45:34 -0500

    change url to create build

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = phase=> {
             if(isDevelopment) {
                 return "https://jsonplaceholder.typicode.com"
             }else{
-                return "https://api.edblog.com/"
+                return "https://jsonplaceholder.typicode.com"
             }
            
         })()
